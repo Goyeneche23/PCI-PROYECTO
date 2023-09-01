553cb0978d6e472138f032b247e02631c52558ff
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2023-09-01 17:52:51 -0400

    Update tablero.py

--- tablero.py
+++ tablero.py
@@ -1,36 +1,35 @@
 class Tablero:
-    def _init_(self):
-        self.num_columnas = 20
-        self.num_filas = 10
-        self.tamaño = 25
-        self.tabla = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            ]
+    def __init__(self):
+        self.num_filas = 20
+        self.num_columnas = 10
+        self.cell_size = 30
+        self.tab = [[0 for j in range(self.num_columnas)] for i in range(self.num_filas)]
+        self.colores = self.seleccion_colores()
 
-    def seleccion_colores_(self):
+    def print_tab(self):
+        for fila in range(self.num_filas):
+            for columna in range(self.num_columnas):
+                print(self.tab[fila][columna], end = " ")
+            print()
+
+    def seleccion_colores(self):
+        Gris = (25, 30, 40)
         Azul = (15, 65, 220)
         Rojo = (230, 20, 20)
         Verde = (50, 230, 25)
         Cyan = (20, 205, 210)
         Morado = (165, 0, 250)
         Amarillo = (235, 235, 5)
+        Blanco = (255, 255, 255)
+
+        return [Gris, Azul, Rojo, Verde, Cyan, Morado, Amarillo, Blanco]
+"""
+    def draw(self):
+        for fila in range(self.num_filas):
+            for columna in range(self.num_columnas):
+                cell value = self.grid[fila][columna]
+                cell_rect = pygame.Rect(columna*self.cell_size, fila*self.cell_size, 
+                self.cell_size, self.cell_size)
+                #pygame.draw.rect(screen, colores, rect)
+                """
+
