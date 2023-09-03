fe0ee8e0f51f86b34761dd1562e7cfa6172d9ee7
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2023-09-02 23:44:19 -0400

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -5,7 +5,6 @@ import pygame, sys
 from tablero import Tablero
 
 pygame.init()
-Azul_fondo = (45, 45, 127)
 
 pantalla = pygame.display.set_mode((350.00, 700.00))
 pygame.display.set_caption("Tetris")
@@ -20,7 +19,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+            
+    Azul_fondo = (45, 45, 127)
     pantalla.fill(Azul_fondo)
     pygame.display.update()
     temporizador.tick(50)
