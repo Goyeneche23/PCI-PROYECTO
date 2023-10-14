56a00bb5939e37f12318bb3d2382be85151506cf
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2023-10-14 01:03:07 -0400

    Update bloques.py

--- bloques.py
+++ bloques.py
@@ -1,38 +1,23 @@
 import pygame
 from Colores import colores
+from blokes import Bloques
 
 class lugar:
-	def __init__(self, row, column):
-		self.row = row
-		self.column = column
+    def __init__(self, fila, columna):
+            self.fila = fila
+            self.columna = columna
 
-class Bloques:
-	def __init__(self, id):
-		self.numero = id
-		self.cells = {}
-	    self.cell_size = 40
-	    self.rotacion = 0
-	    self.color = colores.seleccion_colores()
+class bloque1(Bloques):
+	def __init__(self):
+		super().__init__(id = 1)
+		self.cells = {
+			0: [lugar(0, 2), lugar(1, 0), lugar(1, 1), lugar(1, 2)],
+			1: [lugar(0, 1), lugar(1, 1), lugar(2, 1), lugar(2, 2)],
+			2: [lugar(1, 0), lugar(1, 1), lugar(1, 2), lugar(2, 0)],
+			3: [lugar(0, 0), lugar(0, 1), lugar(1, 1), lugar(2, 1)]
+		}
 
-	def __init__(self, pantalla):
-
-
-	class posicion:
-		def __init__(self, fila, columna):
-			self.fila = fila
-			self.columna = columna
-
-	class bloque1(Bloques):
-		def __init__(self):
-			super().__init__(id = 1)
-			self.cells = {
-				0: [lugar(0, 2), lugar(1, 0), lugar(1, 1), lugar(1, 2)],
-				1: [lugar(0, 1), lugar(1, 1), lugar(2, 1), lugar(2, 2)],
-				2: [lugar(1, 0), lugar(1, 1), lugar(1, 2), lugar(2, 0)],
-				3: [lugar(0, 0), lugar(0, 1), lugar(1, 1), lugar(2, 1)]
-			}
-
-	class bloque2(Bloques):
+class bloque2(Bloques):
 		def __init__(self):
 			super().__init__(id = 2)
 			self.cells = {
@@ -42,7 +27,7 @@ class Bloques:
             	3: [lugar(0, 1), lugar(1, 1), lugar(2, 0), lugar(2, 1)]
 			}
 
-	class bloque3(Bloques):
+class bloque3(Bloques):
 		def __init__(self):
 			super().__init__(id = 3)
 			self.cells = {
@@ -51,3 +36,55 @@ class Bloques:
             	2: [lugar(1, 0), lugar(1, 1), lugar(1, 2), lugar(2, 2)],
             	3: [lugar(0, 1), lugar(1, 1), lugar(2, 0), lugar(2, 1)]
 			}
+
+class bloque4(Bloques):
+		def __init__(self):
+			super().__init__(id = 4)
+			self.cells = {
+            0: [lugar(0, 1), lugar(0, 2), lugar(1, 0), lugar(1, 1)],
+            1: [lugar(0, 1), lugar(1, 1), lugar(1, 2), lugar(2, 2)],
+            2: [lugar(1, 1), lugar(1, 2), lugar(2, 0), lugar(2, 1)],
+            3: [lugar(0, 0), lugar(1, 0), lugar(1, 1), lugar(2, 1)]
+        }
+			
+
+
+class bloque5(Bloques):
+		def __init__(self):
+			super().__init__(id = 5)
+			self.cells = {
+				0: [lugar(0, 0), lugar(1, 0), lugar(0, 1), lugar(1, 1)],
+            	1: [lugar(0, 0), lugar(0, 0), lugar(0, 0), lugar(0, 0)],
+            	2: [lugar(0, 0), lugar(0, 0), lugar(0, 0), lugar(0, 0)],
+            	3: [lugar(0, 0), lugar(0, 0), lugar(0, 0), lugar(0, 0)]
+			}
+
+class bloque6(Bloques):
+		def __init__(self):
+			super().__init__(id = 5)
+			self.cells = {
+	            0: [lugar(0, 1), lugar(1, 0), lugar(1, 1), lugar(1, 2)],
+	            1: [lugar(0, 1), lugar(1, 1), lugar(1, 2), lugar(2, 1)],
+	            2: [lugar(1, 0), lugar(1, 1), lugar(1, 2), lugar(2, 1)],
+	            3: [lugar(0, 1), lugar(1, 0), lugar(1, 1), lugar(2, 1)]
+	        }
+
+class bloque7(Bloques):
+		def __init__(self):
+			super().__init__(id = 7)
+			self.cells = {
+	            0: [lugar(0, 0), lugar(0, 1), lugar(1, 1), lugar(1, 2)],
+	            1: [lugar(0, 2), lugar(1, 1), lugar(1, 2), lugar(2, 1)],
+	            2: [lugar(1, 0), lugar(1, 1), lugar(2, 1), lugar(2, 2)],
+	            3: [lugar(0, 1), lugar(1, 0), lugar(1, 1), lugar(2, 0)]
+	        }
+
+def seleccion_bloque(self):
+    if len(self.bloque) == 0:
+        self.bloque = [Bloque1(), Bloque2(), Bloque3(), Bloque4(), Bloque6(), Bloque7(), Bloque8()]
+    
+    bloque = random.choice(self.bloque)
+    self.bloque.remove(bloque)
+    
+    return bloque
+
