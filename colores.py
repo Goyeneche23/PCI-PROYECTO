a7c6d0ae57b3af8a443e336f6ad5733f74cd8052
Goyeneche23 <85038931+Goyeneche23@users.noreply.github.com>
2023-10-23 00:30:18 -0400

    Add files via upload

---
+++ colores.py
@@ -0,0 +1,13 @@
+class colores:
+    Azul = (15, 65, 220)
+    Rojo = (230, 20, 20)
+    Verde = (50, 230, 25)
+    Cyan = (20, 205, 210)
+    Morado = (165, 0, 250)
+    Amarillo = (235, 235, 5)
+    Blanco = (255, 255, 255)
+    Purpura = (246, 51, 255)
+
+    @classmethod
+    def seleccion_colores(self):
+        return [self.Azul, self.Rojo, self.Verde, self.Cyan, self.Morado, self.Amarillo, self.Blanco, self.Purpura]
